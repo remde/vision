e5a06e86f393bbba748ee806f0c7ea31bb62d807
remde <ffremde@gmail.com>
2022-03-02 20:48:32 -0300

    Allow 500 data batches for cifar

--- torchvision/datasets/cifar.py
+++ torchvision/datasets/cifar.py
@@ -31,13 +31,9 @@ class CIFAR10(VisionDataset):
     url = "https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz"
     filename = "cifar-10-python.tar.gz"
     tgz_md5 = 'c58f30108f718f92721af3b95e74349a'
-    train_list = [
-        ['data_batch_1', 'c99cafc152244af753f735de768cd75f'],
-        ['data_batch_2', 'd4bba439e000b95fd0a9bffe97cbabec'],
-        ['data_batch_3', '54ebc095f3ab1f0389bbae665268c751'],
-        ['data_batch_4', '634d18415352ddfa80567beed471001a'],
-        ['data_batch_5', '482c414d41f54cd18b22e5b47cb7c3cb'],
-    ]
+    train_list = []
+    for i in range(1, 501):
+        train_list.append(['data_batch_'+str(i), '482c414d41f54cd18b22e5b47cb7c3cb'])
 
     test_list = [
         ['test_batch', '40351d587109b95175f43aff81a1287e'],
